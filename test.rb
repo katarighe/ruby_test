4ff90fa40c520a4fe54befeb7087bdf15dc25fc1
katarighe <m.ighe@hotmail.com>
2023-09-27 15:31:36 +0300

    Added all the tests

--- test.rb
+++ test.rb
@@ -1,3 +1,19 @@
+> print "Hello World"
+Hello World => nil
+
+10/3
+10/3.0
+"Happy" + "coding"
+"Happy" + "\n" +  "coding"
+print "Happy" + "\n" +  "coding" + "\n"
+
+a = 3
+a
+a = "Hello"
+a
+b = "World!"
+puts(a + b)
+
 irb(main)> def hello(name)
 irb(main)>   puts("Hello #{name}!")
 irb(main)> end
